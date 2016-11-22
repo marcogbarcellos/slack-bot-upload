ad44255e1c594d99c85870ed5dbcffa261794843
marco <marco@C02RH9ARFVH5.local>
2016-11-21 22:48:42 -0500

    improving code, using nodejslack

--- index.js
+++ index.js
@@ -5,38 +5,56 @@ require('dotenv').config();
 var request = require('request');
 var fs = require('fs');
 var slackBot = require('slackbots');
+var Slack = require('nodejslack');
 
-// Default url slack link to upload files, for more information check: https://api.slack.com/methods/files.upload
-var url_link = 'https://slack.com/api/files.upload?token='+process.env.SLACK_TOKEN;
- 
+// Creating instance to connect to Slack. check: https://github.com/marcogbarcellos/nodejslack
+var slack = new Slack(process.env.SLACK_TOKEN);
+
 // creating a bot, for more options check: https://github.com/mishk0/slack-bot-api
 var bot = new slackBot({
-    token: process.env.SLACK_TOKEN,
-    icon_emoji: ':dog:',
-    name: 'Uploader'
+    token: process.env.SLACK_TOKEN
 });
+
+
 
 bot.on('start', function() { 
 	//Do something when the BOT starts 
+	console.log('BOT started listening...');
 });
 
 // Event to listen all slack messages, to see all possible events check: https://api.slack.com/events-api
 bot.on('message', function(data) {
-	console.log('data:',data);
+	
 	//When someone types "uploadPicture" the bot will upload the file on the #general channel
-	if (data.text == 'uploadPicture') {
-		console.log('uploading..');
+	if (data.text == 'uploadFile') {
 		
-		var formData = {
-		  filename: 'test.csv',
-		  channels: 'general', // If you want to put more than one channel, separate using comma, example: 'general,random'
-		  file: fs.createReadStream('./files/test.csv')
+		var form = {
+		  file: fs.createReadStream('package.json'), // Optional, via multipart/form-data. If omitting this parameter, you MUST submit content
+		  // content: 'Your text here', // Optional, File contents. If omitting this parameter, you must provide a `file` 
+		  filename: 'package.json', // Required 
+		  fileType: 'post', // Optional, See more file types in https://api.slack.com/types/file#file_types
+		  title: 'Title of your file!', // Optional
+		  initial_comment: 'First comment about this file.', // Optional
+		  channels: 'general' //Optional, If you want to put more than one channel, separate using comma, example: 'general,random'
 		};
-		request.post({url: url_link, formData: formData}, function optionalCallback(err, httpResponse, body) {
-		  if (err) {
-		    return console.error('upload failed:', err);
-		  }
-		  console.log('Upload successful!  Server responded with:', body);
+
+		slack.fileUpload(form)
+		.then(function(response){
+
+			// Slack sends a json with a boolean var ok. 
+			// Error example : data = { ok: false, error: 'user_not_found' }
+			// Error example : data = { ok: true, file: 'user_not_found' }
+			if(!response || !response.ok){
+				return Promise.reject(new Error('Something wrong happened during the upload.'));
+			}
+			console.log('Uploaded Successfully:',response);
+
+			return Promise.resolve(response);
+		})
+		.catch(function(err){
+			console.log('Failed on Uploading:',err);
+
+			return Promise.reject(err);
 		});
 	}
 });
